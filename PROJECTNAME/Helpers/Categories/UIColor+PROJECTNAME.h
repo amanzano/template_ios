3d640f518d8490d9aaf931ffaa99a661c44ab0c9
Audrey Manzano <audrey.manzano@vivant.com.au>
2015-09-18 12:45:04 +1000

    Change project file architecture

---
+++ PROJECTNAME/Helpers/Categories/UIColor+PROJECTNAME.h
@@ -0,0 +1,12 @@
+//
+//  UIColor+PROJECTNAME.h
+//  PROJECTNAME
+//
+//  Copyright (c) 2015 Vivant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (PROJECTNAME)
+
+@end
---
+++ PROJECTNAME/Helpers/Categories/UIColor+PROJECTNAME.m
@@ -0,0 +1,12 @@
+//
+//  UIColor+PROJECTNAME.m
+//  PROJECTNAME
+//
+//  Copyright (c) 2015 Vivant. All rights reserved.
+//
+
+#import "UIColor+PROJECTNAME.h"
+
+@implementation UIColor (PROJECTNAME)
+
+@end
